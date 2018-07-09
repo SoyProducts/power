6422059405685731b7653f37bc8d1f4b4e82ce0d
Andrew Tae <tiffanyw.hu@gmail.com>
2018-07-08 22:38:13 -0700

    why didn't you work before

--- crowtits-backend/app/models/user.rb
+++ crowtits-backend/app/models/user.rb
@@ -1,19 +1,7 @@
-require 'devise_token_auth'
-
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-   def self.create_user_for_google(data)
-   where(uid: data["email"]).first_or_initialize.tap do |user|
-     user.provider="google_oauth2"
-     user.uid=data["email"]
-     user.email=data["email"]
-     user.password=Devise.friendly_token[0,20]
-     user.password_confirmation=user.password
-     user.save!
-   end
-  end
+  include DeviseTokenAuth::Concerns::User
 end
--- crowtits-backend/config/routes.rb
+++ crowtits-backend/config/routes.rb
@@ -3,7 +3,7 @@ require 'devise_token_auth'
 # devise_for :users
 Rails.application.routes.draw do
   post 'auth/request', to:'authorization#get_authorization'
-  mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+  # mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 # mount_devise_token_auth_for 'User', at: 'auth'
