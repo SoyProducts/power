ff42607dcfd5892d8ba47d41a26067bfa85b0518
Andrew Tae <tiffanyw.hu@gmail.com>
2018-07-08 22:03:39 -0700

    setting cookies to the table

--- crowtits/src/App.js
+++ crowtits/src/App.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
+import {GoogleLogin, GoogleLogout} from 'react-google-oauth'
 import logo from './logo.svg';
 import './App.css';
+import Cookies from 'universal-cookie'
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.responseGoogle = this.responseGoogle.bind(this)
+  }
+
+  responseGoogle(google_response) {
+    var token = google_response.Zi;
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${google_response.Zi.accessToken}`,
+        'Content-Type': 'application/json',
+        'access_token': `${google_response.Zi.accessToken}`
+      },
+      body: JSON.stringify(token)
+    }
+
+    return fetch(`http://localhost:3001/auth/sign_in`, requestOptions)
+      .then(response => {
+        let cookie = new Cookies()
+        cookie.set('accesstoken', response.headers.get('access-token'));
+        cookie.set('client',response.headers.get('client'));
+        cookie.set('tokentype',response.headers.get('token-type'));
+        cookie.set('expiry',response.headers.get('expiry'));
+        cookie.set('uid', response.headers.get('uid'));
+      })
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +44,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <GoogleLogin onLoginSuccess={this.responseGoogle} onFailure={this.responseGoogle}/>
+        <GoogleLogout onLoginSuccess={this.responseGoogle} onFailure={this.responseGoogle}/>
       </div>
     );
   }
--- crowtits/src/index.js
+++ crowtits/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {GoogleAPI} from 'react-google-oauth'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<GoogleAPI clientId="639592487963-akhi4ormq2ipqc6r5r5m8sfrpjsa50g9.apps.googleusercontent.com">
+        <App /></GoogleAPI>, document.getElementById('root'));
 registerServiceWorker();
